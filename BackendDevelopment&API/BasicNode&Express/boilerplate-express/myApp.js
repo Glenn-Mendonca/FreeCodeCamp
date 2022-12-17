7488db34262a3ec7a0fb99c58b59a26baa153bc5
Glenn Mendonca <glenm.code@gmail.com>
2022-12-17 10:58:22 +0530

    Serve JSON on a Specific Route

--- BackendDevelopment&API/BasicNode&Express/boilerplate-express/myApp.js
+++ BackendDevelopment&API/BasicNode&Express/boilerplate-express/myApp.js
@@ -2,10 +2,16 @@ let express = require("express");
 let path = require("path");
 let app = express();
 
-app.use(path.join(__dirname, "public"), express.static());
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json",
+  });
 });
 
 module.exports = app;
