70146344d2574371ecffc36770c48dd591680625
Glenn Mendonca <glenm.code@gmail.com>
2022-12-17 10:49:23 +0530

    Serve an HTML File

--- BackendDevelopment&API/BasicNode&Express/boilerplate-express/myApp.js
+++ BackendDevelopment&API/BasicNode&Express/boilerplate-express/myApp.js
@@ -1,8 +1,9 @@
 let express = require("express");
+let path = require("path");
 let app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 module.exports = app;
