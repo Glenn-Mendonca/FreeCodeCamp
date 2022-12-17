5fd511aabab756b7036750bfe79022e52e3c70de
Glenn Mendonca <glenm.code@gmail.com>
2022-12-17 10:53:05 +0530

    Serve Static Assets

--- BackendDevelopment&API/BasicNode&Express/boilerplate-express/myApp.js
+++ BackendDevelopment&API/BasicNode&Express/boilerplate-express/myApp.js
@@ -2,6 +2,8 @@ let express = require("express");
 let path = require("path");
 let app = express();
 
+app.use(path.join(__dirname, "public"), express.static());
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
